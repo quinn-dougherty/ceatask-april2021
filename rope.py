5148538b653b54e73fc5d48e910115397a916772
Quinn Dougherty <quinn.dougherty92@gmail.com>
2021-04-20 15:56:04 +0100

    black autoformatter

--- rope.py
+++ rope.py
@@ -1,167 +1,194 @@
 from typing import Tuple, Callable
 
+
 class Rope:
-  def __init__(self, text):
-    self.text = text
-    self.size = len(text)
-    self.left = None
-    self.right = None
-  def __eq__(self, other) -> bool:
-    return all((
-      self.text == other.text,
-      self.size == other.size,
-      self.left == other.left,
-      self.right == other.right
-    ))
-  # prints contents including showing the hierarchy
-  # it's not required for this function to work, it's just there to help with debugging
-  # 
-  # e.g. if the  root node has ABC, the left node has DEF, and the right node has GHI, 
-  # the output will look like:
-  # -DEF
-  # ABC
-  # -GHI
-  def to_string_debug(self, indentLevel = 0):
-    leftText = self.left.to_string_debug(indentLevel + 1) if self.left else ''
-    rightText = self.right.to_string_debug(indentLevel + 1) if self.right else ''
-    return leftText + ('-'*indentLevel) + self.text + '\n' + rightText
+    def __init__(self, text):
+        self.text = text
+        self.size = len(text)
+        self.left = None
+        self.right = None
 
-  # just prints the stored text
-  def to_string(self):
-    leftText =  self.left.to_string() if self.left else  ''
-    rightText = self.right.to_string() if self.right else  ''
-    return leftText + self.text + rightText
+    def __eq__(self, other) -> bool:
+        return all(
+            (
+                self.text == other.text,
+                self.size == other.size,
+                self.left == other.left,
+                self.right == other.right,
+            )
+        )
 
-  # How long the text stored is in all of the children combined
-  # This is the same as this.to_string().length
-  def total_size(self):
-    leftText =  self.left.total_size() if self.left else  0
-    rightText = self.right.total_size() if self.right else  0
-    return leftText + len(self.text) + rightText
+    # prints contents including showing the hierarchy
+    # it's not required for this function to work, it's just there to help with debugging
+    #
+    # e.g. if the  root node has ABC, the left node has DEF, and the right node has GHI,
+    # the output will look like:
+    # -DEF
+    # ABC
+    # -GHI
+    def to_string_debug(self, indentLevel=0):
+        leftText = self.left.to_string_debug(indentLevel + 1) if self.left else ""
+        rightText = self.right.to_string_debug(indentLevel + 1) if self.right else ""
+        return leftText + ("-" * indentLevel) + self.text + "\n" + rightText
 
-  # how deep the tree is (I.e. the maximum depth of children)
-  def depth(self):
-    return 1 + max(self.left_depth(), self.right_depth())
+    # just prints the stored text
+    def to_string(self):
+        leftText = self.left.to_string() if self.left else ""
+        rightText = self.right.to_string() if self.right else ""
+        return leftText + self.text + rightText
 
-  # Whether the rope is balanced, i.e. whether any subtrees have branches
-  # which differ by more than one in depth. 
-  def is_balanced(self): 
-    leftBalanced =  self.left.is_balanced() if self.left else True
-    rightBalanced = self.right.is_balanced() if self.right else True
+    # How long the text stored is in all of the children combined
+    # This is the same as this.to_string().length
+    def total_size(self):
+        leftText = self.left.total_size() if self.left else 0
+        rightText = self.right.total_size() if self.right else 0
+        return leftText + len(self.text) + rightText
 
-    return leftBalanced and rightBalanced and abs(self.left_depth() - self.right_depth()) < 2
+    # how deep the tree is (I.e. the maximum depth of children)
+    def depth(self):
+        return 1 + max(self.left_depth(), self.right_depth())
 
-  def left_depth(self): 
-    if (not self.left): 
-      return 0
-    return self.left.depth()
+    # Whether the rope is balanced, i.e. whether any subtrees have branches
+    # which differ by more than one in depth.
+    def is_balanced(self):
+        leftBalanced = self.left.is_balanced() if self.left else True
+        rightBalanced = self.right.is_balanced() if self.right else True
 
-  def right_depth(self): 
-    if (not self.right): 
-      return 0
-    return self.right.depth()
+        return (
+            leftBalanced
+            and rightBalanced
+            and abs(self.left_depth() - self.right_depth()) < 2
+        )
 
-  # Helper method which converts the rope into an associative array
-  # 
-  # Only used for debugging, this has no functional purpose
-  def to_dictionary(self):
-    mapVersion = {
-      'text': self.text
-    }
-    if (self.right):
-      mapVersion['right'] = self.right.to_dictionary()
-    if (self.left):
-      mapVersion['left'] = self.left.to_dictionary()
-    return mapVersion
+    def left_depth(self):
+        if not self.left:
+            return 0
+        return self.left.depth()
+
+    def right_depth(self):
+        if not self.right:
+            return 0
+        return self.right.depth()
+
+    # Helper method which converts the rope into an associative array
+    #
+    # Only used for debugging, this has no functional purpose
+    def to_dictionary(self):
+        mapVersion = {"text": self.text}
+        if self.right:
+            mapVersion["right"] = self.right.to_dictionary()
+        if self.left:
+            mapVersion["left"] = self.left.to_dictionary()
+        return mapVersion
+
 
 def create_rope_from_map(map):
-  rope = Rope(map['text'])
-  if 'left' in map and map["left"] is not None:
-    if isinstance(map["left"], Rope):
-      rope.left = map["left"]
-    else:
-      rope.left = create_rope_from_map(map['left'])
-  if 'right' in map and map["right"] is not None:
-    if isinstance(map["right"], Rope):
-      rope.right = map["right"]
-    else:
-      rope.right = create_rope_from_map(map['right'])
-  return rope
-
-def prepend(rope, text): 
-  if (rope.left):
-    prepend(rope.left, text)
-  else:
-    rope.left = Rope(text)
-  
-  return rope
-
-def append(rope, text): 
-  if (rope.right):
-    append(rope.right, text)
-  else:
-    rope.right = Rope(text)
-  
-  return rope
-
-def fmap_rope(f: Callable[[str], str], rope: Rope) -> Rope:
-  if rope is None:
+    rope = Rope(map["text"])
+    if "left" in map and map["left"] is not None:
+        if isinstance(map["left"], Rope):
+            rope.left = map["left"]
+        else:
+            rope.left = create_rope_from_map(map["left"])
+    if "right" in map and map["right"] is not None:
+        if isinstance(map["right"], Rope):
+            rope.right = map["right"]
+        else:
+            rope.right = create_rope_from_map(map["right"])
     return rope
 
-  return create_rope_from_map({
-    "text": f(rope.text),
-    "left": fmap_rope(f, rope.left) if rope.left else None,
-    "right": fmap_rope(f, rope.right) if rope.right else None
-  })
+
+def prepend(rope, text):
+    if rope.left:
+        prepend(rope.left, text)
+    else:
+        rope.left = Rope(text)
+
+    return rope
+
+
+def append(rope, text):
+    if rope.right:
+        append(rope.right, text)
+    else:
+        rope.right = Rope(text)
+
+    return rope
+
+
+def fmap_rope(f: Callable[[str], str], rope: Rope) -> Rope:
+    if rope is None:
+        return rope
+
+    return create_rope_from_map(
+        {
+            "text": f(rope.text),
+            "left": fmap_rope(f, rope.left) if rope.left else None,
+            "right": fmap_rope(f, rope.right) if rope.right else None,
+        }
+    )
 
 
 def concat_right(rope: Rope, other: Rope) -> Rope:
-  if rope.right is not None:
-    concat_right(rope.right, other)
-  else:
-    rope.right = other
-  return rope
+    if rope.right is not None:
+        concat_right(rope.right, other)
+    else:
+        rope.right = other
+    return rope
+
 
 def concat2(rope: Rope, other: Rope) -> Rope:
-  new_rope = Rope("")
-  new_rope.left = rope
-  new_rope.right = other
-  return new_rope
+    new_rope = Rope("")
+    new_rope.left = rope
+    new_rope.right = other
+    return new_rope
 
-# This is an internal API. You can implement it however you want. 
+
+# This is an internal API. You can implement it however you want.
 # (E.g. you can choose to mutate the input rope or not)
 def split_at_naive(rope: Rope, position: int) -> Tuple[Rope, Rope]:
-  """First iteration: kills structure of rope, always returns a 3-node rope.
+    """First iteration: kills structure of rope, always returns a 3-node rope.
   """
-  text = rope.to_string()
-  length = rope.total_size()
-  assert position <= length, "position requested is larger than total length of rope"
-  new_left = Rope(text[:position])
-  right = Rope(text[position:])
-  return (new_left, right)
+    text = rope.to_string()
+    length = rope.total_size()
+    assert position <= length, "position requested is larger than total length of rope"
+    new_left = Rope(text[:position])
+    right = Rope(text[position:])
+    return (new_left, right)
+
+
+def delete_range_naive(rope: Rope, start: int, end: int) -> Rope:
+    left, _ = split_at_naive(rope, start)
+    _, right = split_at_naive(rope, end)
+    return concat2(left, right)
+
+
+def insert_naive(rope: Rope, text: str, location: int) -> Rope:
+    left, right = split_at_naive(rope, location)
+    new_rope = Rope(text)
+    new_rope.left = left
+    new_rope.right = right
+    return new_rope
+
 
 def split_at(rope: Rope, position: int) -> Tuple[Rope, Rope]:
-  # TODO
-  return new_left, right
+    # TODO
+    return new_left, right
+
 
 def delete_range(rope: Rope, start: int, end: int) -> Rope:
-  left, _ = split_at_naive(rope, start)
-  _, right = split_at_naive(rope, end)
-  return concat2(left, right)
+    pass
 
 
-def insert(rope, text, location): 
-  left, right = split_at_naive(rope, location)
-  new_rope = Rope(text)
-  new_rope.left = left
-  new_rope.right = right
-  return new_rope
+def insert(rope: Rope, text: str, location: int) -> Rope:
+    pass
+
 
 def rebalance(rope):
-  # TODO
-  return rope
+    # TODO
+    return rope
 
-'''
+
+"""
  Rotates a tree: used for rebalancing.
 
  Turns:
@@ -175,15 +202,18 @@ def rebalance(rope):
    b
   /
 a
-'''
-def rotate_left(rope):
-  newParent = rope.right
-  newLeft = rope
-  newLeft.right = newParent.left
-  newParent.left = newLeft
-  return newParent
+"""
 
-'''
+
+def rotate_left(rope):
+    newParent = rope.right
+    newLeft = rope
+    newLeft.right = newParent.left
+    newParent.left = newLeft
+    return newParent
+
+
+"""
 /*
  Rotates a tree: used for rebalancing.
 
@@ -198,10 +228,12 @@ def rotate_left(rope):
        b
         \
          c 
-'''
+"""
+
+
 def rotate_right(rope):
-  newParent = rope.left
-  newRight = rope
-  newRight.left = newParent.right
-  newParent.right = newRight
-  return newParent
+    newParent = rope.left
+    newRight = rope
+    newRight.left = newParent.right
+    newParent.right = newRight
+    return newParent
--- rope_test.py
+++ rope_test.py
@@ -1,102 +1,169 @@
 from rope import (
-    Rope, prepend, append, delete_range, insert,
-    create_rope_from_map, rotate_left, rotate_right, rebalance, split_at_naive,
+    Rope,
+    prepend,
+    append,
+    delete_range,
+    insert,
+    create_rope_from_map,
+    rotate_left,
+    rotate_right,
+    rebalance,
+    split_at_naive,
     fmap_rope,
+    insert_naive,
+    delete_range_naive,
 )
 import unittest
 
 # These tests are here as a starting point, they are not comprehensive
 class Testing(unittest.TestCase):
     def test_rope_basics(self):
-        self.assertEqual(Rope('test').to_string(), 'test')
-        self.assertEqual(prepend(Rope('test'), 'abc').to_string(), 'abctest')
-        self.assertEqual(append(Rope('test'), 'abc').to_string(), 'testabc')
-        
+        self.assertEqual(Rope("test").to_string(), "test")
+        self.assertEqual(prepend(Rope("test"), "abc").to_string(), "abctest")
+        self.assertEqual(append(Rope("test"), "abc").to_string(), "testabc")
+
+    def test_deletion_naive(self):
+        self.assertEqual(delete_range_naive(Rope("test"), 1, 2).to_string(), "tst")
+        self.assertEqual(delete_range_naive(Rope("test"), 2, 4).to_string(), "te")
+        self.assertEqual(delete_range_naive(Rope("test"), 0, 2).to_string(), "st")
+
+    def test_insertion_naive(self):
+        self.assertEqual(insert_naive(Rope("test"), "123", 2).to_string(), "te123st")
+        self.assertEqual(insert_naive(Rope("test"), "123", 4).to_string(), "test123")
+        self.assertEqual(insert_naive(Rope("test"), "123", 0).to_string(), "123test")
+
     def test_deletion(self):
-        self.assertEqual(delete_range(Rope('test'), 1, 2).to_string(), 'tst')
-        self.assertEqual(delete_range(Rope('test'), 2, 4).to_string(), 'te')
-        self.assertEqual(delete_range(Rope('test'), 0, 2).to_string(), 'st')
-        
+        self.assertEqual(delete_range(Rope("test"), 1, 2).to_string(), "tst")
+        self.assertEqual(delete_range(Rope("test"), 2, 4).to_string(), "te")
+        self.assertEqual(delete_range(Rope("test"), 0, 2).to_string(), "st")
+
     def test_insertion(self):
-        self.assertEqual(insert(Rope('test'), '123', 2).to_string(), 'te123st')
-        self.assertEqual(insert(Rope('test'), '123', 4).to_string(), 'test123')
-        self.assertEqual(insert(Rope('test'), '123', 0).to_string(), '123test')
+        self.assertEqual(insert(Rope("test"), "123", 2).to_string(), "te123st")
+        self.assertEqual(insert(Rope("test"), "123", 4).to_string(), "test123")
+        self.assertEqual(insert(Rope("test"), "123", 0).to_string(), "123test")
 
     def test_split_at_naive(self):
         test = Rope("test")
         self.assertEqual(split_at_naive(test, 2), (Rope("te"), Rope("st")))
 
     def test_fmap_rope(self):
-        test = create_rope_from_map({
-            'text': '3',
-        'left': { 'text': 'a' },
-        'right': { 'text': '5', 'left': { 'text': 'b' }, 'right': { 'text': '7', 'left': { 'text': 'c' }, 'right': { 'text': 'd' } } },
-        })
-        test_upper = create_rope_from_map({
-            'text': '3',
-        'left': { 'text': 'A' },
-        'right': { 'text': '5', 'left': { 'text': 'B' }, 'right': { 'text': '7', 'left': { 'text': 'C' }, 'right': { 'text': 'D' } } },
-        })
+        test = create_rope_from_map(
+            {
+                "text": "3",
+                "left": {"text": "a"},
+                "right": {
+                    "text": "5",
+                    "left": {"text": "b"},
+                    "right": {
+                        "text": "7",
+                        "left": {"text": "c"},
+                        "right": {"text": "d"},
+                    },
+                },
+            }
+        )
+        test_upper = create_rope_from_map(
+            {
+                "text": "3",
+                "left": {"text": "A"},
+                "right": {
+                    "text": "5",
+                    "left": {"text": "B"},
+                    "right": {
+                        "text": "7",
+                        "left": {"text": "C"},
+                        "right": {"text": "D"},
+                    },
+                },
+            }
+        )
         self.assertEqual(fmap_rope(lambda s: s.upper(), test), test_upper)
 
     def test_extra_credit_rebalancing(self):
-      self.assertEqual(rotate_left(create_rope_from_map({
-        'text': '3', 
-        'left': { 'text': 'a' }, 
-        'right': { 'text': '5', 'left': { 'text': 'b' }, 'right': { 'text': '7', 'left': { 'text': 'c' }, 'right': { 'text': 'd' } } }, 
-      })).to_dictionary(), {
-        'text': '5', 
-        'left': {
-          'text': '3', 
-          'left': { 'text': 'a' }, 
-          'right': { 'text': 'b' }
-        }, 
-        'right': {
-          'text': '7', 
-          'left': { 'text': 'c' }, 
-          'right': { 'text': 'd' }
-        }, 
-      })
-      self.assertEqual(rotate_right(create_rope_from_map({
-        'text': '5', 
-        'left': { 'text': '3', 'right': { 'text': 'b' }, 'left': { 'text': '2', 'left': { 'text': 'd' }, 'right': { 'text': 'c' } } }, 
-        'right': { 'text': 'a' }, 
-      })).to_dictionary(), {
-        'text': '3', 
-        'left': {
-          'text': '2', 
-          'left': { 'text': 'd' }, 
-          'right': { 'text': 'c' }
-        }, 
-        'right': {
-          'text': '5', 
-          'left': { 'text': 'b' }, 
-          'right': { 'text': 'a' }
-        }, 
-      })
+        self.assertEqual(
+            rotate_left(
+                create_rope_from_map(
+                    {
+                        "text": "3",
+                        "left": {"text": "a"},
+                        "right": {
+                            "text": "5",
+                            "left": {"text": "b"},
+                            "right": {
+                                "text": "7",
+                                "left": {"text": "c"},
+                                "right": {"text": "d"},
+                            },
+                        },
+                    }
+                )
+            ).to_dictionary(),
+            {
+                "text": "5",
+                "left": {"text": "3", "left": {"text": "a"}, "right": {"text": "b"}},
+                "right": {"text": "7", "left": {"text": "c"}, "right": {"text": "d"}},
+            },
+        )
+        self.assertEqual(
+            rotate_right(
+                create_rope_from_map(
+                    {
+                        "text": "5",
+                        "left": {
+                            "text": "3",
+                            "right": {"text": "b"},
+                            "left": {
+                                "text": "2",
+                                "left": {"text": "d"},
+                                "right": {"text": "c"},
+                            },
+                        },
+                        "right": {"text": "a"},
+                    }
+                )
+            ).to_dictionary(),
+            {
+                "text": "3",
+                "left": {"text": "2", "left": {"text": "d"}, "right": {"text": "c"}},
+                "right": {"text": "5", "left": {"text": "b"}, "right": {"text": "a"}},
+            },
+        )
 
-      balancedTree = {
-        'text': 'b', 
-        'left': { 'text': 'a' }, 
-        'right': { 'text': 'c' }
-      }
+        balancedTree = {"text": "b", "left": {"text": "a"}, "right": {"text": "c"}}
 
-      self.assertEqual(rebalance(create_rope_from_map({
-        'text': 'c', 
-        'left': { 'text': 'a', 'right': { 'text': 'b' } }, 
-      })).to_dictionary(), balancedTree)
-      self.assertEqual(rebalance(create_rope_from_map({
-        'text': 'c', 
-        'left': { 'text': 'b', 'left': { 'text': 'a' } }, 
-      })).to_dictionary(), balancedTree)
-      self.assertEqual(rebalance(create_rope_from_map({
-        'text': 'a', 
-        'right': { 'text': 'b', 'right': { 'text': 'c' } }, 
-      })).to_dictionary(), balancedTree)
-      self.assertEqual(rebalance(create_rope_from_map({
-        'text': 'a', 
-        'right': { 'text': 'c', 'left': { 'text': 'b' } }, 
-      })).to_dictionary(), balancedTree)
+        self.assertEqual(
+            rebalance(
+                create_rope_from_map(
+                    {"text": "c", "left": {"text": "a", "right": {"text": "b"}},}
+                )
+            ).to_dictionary(),
+            balancedTree,
+        )
+        self.assertEqual(
+            rebalance(
+                create_rope_from_map(
+                    {"text": "c", "left": {"text": "b", "left": {"text": "a"}},}
+                )
+            ).to_dictionary(),
+            balancedTree,
+        )
+        self.assertEqual(
+            rebalance(
+                create_rope_from_map(
+                    {"text": "a", "right": {"text": "b", "right": {"text": "c"}},}
+                )
+            ).to_dictionary(),
+            balancedTree,
+        )
+        self.assertEqual(
+            rebalance(
+                create_rope_from_map(
+                    {"text": "a", "right": {"text": "c", "left": {"text": "b"}},}
+                )
+            ).to_dictionary(),
+            balancedTree,
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
